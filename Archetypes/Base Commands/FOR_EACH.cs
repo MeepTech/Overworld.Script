f848429ec76807f3d05d7e55a44b9b4e419ba2f1
meep@UNIT-01 <meepdottech@gmail.com>
2022-01-27 00:54:16 -0500

    implementing simple ux in unity

---
+++ Archetypes/Base Commands/FOR_EACH.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Overworld.Script {
+
+  public partial class Ows {
+    public partial class Command {
+
+      /// <summary>
+      /// A loop that executes on each item in a object collection.
+      /// You can substitute in 'LOOP-OBJECT' for any of the params of the command that will be called for it to execute on the current loop object.
+      /// </summary>
+      public class FOR_EACH : Ows.Command.Type {
+
+        FOR_EACH()
+          : base(
+              new("FOR-EACH"),
+              new[] {
+                typeof(Ows.Collection),
+                typeof(Command)
+              }
+            ) {
+        }
+
+        public override Func<Context, Variable> Execute {
+          get;
+        } = context => {
+          throw new System.NotImplementedException($"FOR-EACH and LOOP-OBJECT not yet implimented");
+        };
+      }
+    }
+  }
+}
